71d66bd4b497b60c884efe0ceb87e9afb39a5fa9
HenryAnyiam <anyiamhenry7@gmail.com>
2024-12-04 13:46:56 +0100

    Add quick route

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('login/', auth_user, name='login-user'),
     path('profile/', getUserProfile, name= 'users-profile'),
     path('staff/', StaffView.as_view(), name='staffs'),
-    path('admin/' create_admin, name='create-user'),
+    path('admin/', create_admin, name='create-user'),
 
 
 
